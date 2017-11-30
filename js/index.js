61077c41f45e9cba7a88bba8241e73e5d8ba0386
Viosey <i@viosey.com>
2017-12-01 02:16:12 +0800

    update: add monero(xmr) mining

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ $('.slider').fullpage({
 	paddingTop: 85,
 	navigation: true,
 	navigationPosition: 'right',
-	onLeave: function(index, newIndex, direction){
-		$('.slide-'+newIndex).addClass('is-active');
+	onLeave: function(index, newIndex, direction) {
+		$('.slide-' + newIndex).addClass('is-active');
 
 		if (newIndex == 5) {
 			$('#fp-nav').addClass('slide-5-active');
@@ -11,16 +11,16 @@ $('.slider').fullpage({
 			$('#fp-nav').removeClass('slide-5-active');
 		}
 	},
-	afterLoad: function(anchorLink, index){
+	afterLoad: function(anchorLink, index) {
 		$('.slide-1').addClass('is-active');
-	},
+	}
 });
 
-function iconHovers(){
+function iconHovers() {
 	var $icons = $('.slide-icons > span');
 
 	$icons.on({
-		mouseenter: function(){
+		mouseenter: function() {
 			var $this = $(this);
 
 			if (!$this.hasClass('active-icon')) {
@@ -29,97 +29,120 @@ function iconHovers(){
 					thisID = $this.data('id');
 
 				$activeIcon.removeClass('active-icon');
-				$activeIcon.parent().find('.icon-text [data-id="'+activeID+'"]').removeClass('active-icon');
+				$activeIcon
+					.parent()
+					.find('.icon-text [data-id="' + activeID + '"]')
+					.removeClass('active-icon');
 
 				$this.addClass('active-icon');
-				$this.parent().find('.icon-text [data-id="'+thisID+'"]').addClass('active-icon');
+				$this
+					.parent()
+					.find('.icon-text [data-id="' + thisID + '"]')
+					.addClass('active-icon');
 			}
 		}
-	})
-};
+	});
+}
 
 iconHovers();
 
-function menuToggle(){
+function menuToggle() {
 	var $button = $('.site-menu-button'),
 		$menu = $('.site-menu-wrap');
 
 	$button.on({
-		click: function(){
-
+		click: function() {
 			$(this).toggleClass('menu-active');
 			$menu.toggleClass('menu-active');
 		},
-		mouseenter: function(){
+		mouseenter: function() {
 			$menu.addClass('menu-hovered');
 		},
-		mouseleave: function(){
+		mouseleave: function() {
 			$menu.removeClass('menu-hovered');
 		}
-	})
-};
+	});
+}
 
 menuToggle();
 
 $('.nav-shortcut').on({
-	mouseenter: function(){
+	mouseenter: function() {
 		$(this).addClass('sc-hovered');
 	},
-	mouseleave: function(){
+	mouseleave: function() {
 		$(this).removeClass('sc-hovered');
 	}
 });
 
 $('.panel').on({
-	mouseenter: function(){
+	mouseenter: function() {
 		$('.page-header').css('z-index', '0');
 		$(this).addClass('hovered');
 	},
-	mouseleave: function(){
+	mouseleave: function() {
 		$('.page-header').css('z-index', '200');
 		$(this).removeClass('hovered');
 	}
-})
+});
 
 $('.site-menu-button').mouseenter(function() {
-  $( ".site-menu-text" ).css( "opacity", "1" );
+	$('.site-menu-text').css('opacity', '1');
 });
 $('.site-menu-button').mouseleave(function() {
-  $( ".site-menu-text" ).css( "opacity", "0" );
+	$('.site-menu-text').css('opacity', '0');
 });
 
+function loadBlogFeed() {
+	var xml = $.ajax({ url: 'https://blog.viosey.com/atom.xml', async: false }),
+		xmlDoc = $.parseXML(xml.responseText),
+		$xml = $(xmlDoc);
 
-function loadBlogFeed(){
-  var xml=$.ajax({url:"https://blog.viosey.com/atom.xml", async:false}),
-    xmlDoc = $.parseXML(xml.responseText),
-    $xml = $( xmlDoc );
+	$xml.find('entry').each(function(index) {
+		var title = $(this)
+			.find('title')
+			.text();
+		var link = $(this)
+			.find('link')
+			.attr('href');
+		var published = $(this)
+			.find('published')
+			.text()
+			.substr(0, 10);
+		var summary = $(this)
+			.find('summary')
+			.text();
+		var $summary = $.parseHTML(
+			$(this)
+				.find('summary')
+				.text()
+		);
 
-  $xml.find( "entry" ).each(function(index){
-      var title = $(this).find('title').text();
-      var link = $(this).find('link').attr('href');
-      var published = $(this).find('published').text().substr(0, 10);
-      var summary = $(this).find('summary').text();
-      var $summary = $.parseHTML($(this).find('summary').text());
-
-      $(".news-title").eq(index).append(title);
-      $(".news-item").eq(index).attr("href", link);
-      $(".news-date").eq(index).append(published);
-      $(".news-summary").eq(index).append($summary[1]);
-      if (index == 5) return false;
-  });
+		$('.news-title')
+			.eq(index)
+			.append(title);
+		$('.news-item')
+			.eq(index)
+			.attr('href', link);
+		$('.news-date')
+			.eq(index)
+			.append(published);
+		$('.news-summary')
+			.eq(index)
+			.append($summary[1]);
+		if (index == 5) return false;
+	});
 }
-$(".site-menu-icon").click(function() {
-
-})
+$('.site-menu-icon').click(function() {});
 // $(document).ready(function(){
 //   console.log(1);
 // });
 
-window.onload = function(){
-  loadBlogFeed();
-  console.info(
-      "\n %c Copyrightight © viosey 2014-2017 %c \n\n",
-      "color:#455a64;background:#e0e0e0;padding:5px 0;border-top-left-radius:5px;border-bottom-left-radius:5px;",
-      "color:#ffffff;background:##455a64;padding:5px 0;border-top-right-radius:5px;border-bottom-right-radius:5px;"
-  );
+window.onload = function() {
+	loadBlogFeed();
+	console.info(
+		'\n %c Copyrightight © viosey 2014-2017 %c \n\n',
+		'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-left-radius:5px;border-bottom-left-radius:5px;',
+		'color:#ffffff;background:##455a64;padding:5px 0;border-top-right-radius:5px;border-bottom-right-radius:5px;'
+	);
 };
